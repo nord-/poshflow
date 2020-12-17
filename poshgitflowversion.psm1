e9d793e379bad2a109836d128df2549c2afb8b2d
Rickard Nord <rickard.nord@combitech.se>
2020-12-17 08:26:19 +0100

    feat: enable not stating branch name on complete

--- poshgitflowversion.psm1
+++ poshgitflowversion.psm1
@@ -1,6 +1,54 @@
 $m = "master"
 $d = "develop"
 
+Function Pause ($Message = "Press any key to continue . . . ") {
+    If ($psISE) {
+        # The "ReadKey" functionality is not supported in Windows PowerShell ISE.
+ 
+        $Shell = New-Object -ComObject "WScript.Shell"
+        $Button = $Shell.Popup("Click OK to continue.", 0, "Script Paused", 0)
+ 
+        Return
+    }
+ 
+    Write-Host -NoNewline $Message
+ 
+    $Ignore =
+        16,  # Shift (left or right)
+        17,  # Ctrl (left or right)
+        18,  # Alt (left or right)
+        20,  # Caps lock
+        91,  # Windows key (left)
+        92,  # Windows key (right)
+        93,  # Menu key
+        144, # Num lock
+        145, # Scroll lock
+        166, # Back
+        167, # Forward
+        168, # Refresh
+        169, # Stop
+        170, # Search
+        171, # Favorites
+        172, # Start/Home
+        173, # Mute
+        174, # Volume Down
+        175, # Volume Up
+        176, # Next Track
+        177, # Previous Track
+        178, # Stop Media
+        179, # Play
+        180, # Mail
+        181, # Select Media
+        182, # Application 1
+        183  # Application 2
+ 
+    While ($KeyInfo.VirtualKeyCode -Eq $Null -Or $Ignore -Contains $KeyInfo.VirtualKeyCode) {
+        $KeyInfo = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+    }
+ 
+    Write-Host
+}
+
 function Set-Branch {
     [cmdletbinding()]
     param(
@@ -108,7 +156,11 @@ function Start-Feature {
 function Start-HotFix {
     process {
         Set-Branch $m
-        $version = (gitversion | convertFrom-json)
+        try {
+            $version = (gitversion /verbosity Quiet /nofetch /output json | convertFrom-json)
+        } catch {
+            return
+        }
         $major = [int]$version.Major
         $minor = [int]$version.Minor
         $patch = [int]$version.Patch + 1
@@ -125,14 +177,29 @@ function Complete-HotFix {
     [cmdletbinding()]
     param
     (
-        [Parameter(Mandatory=$true)]$hotfixBranch
+        [Parameter(Mandatory=$false)]$hotfixBranch
     )
     process { 
+        if ($hotfixBranch -eq $null) {
+            $currentBranch = (git branch --show-current)
+            $names = $currentBranch -split "/"
+            if ($names[0] -eq "hotfix") {
+                $hotfixBranch = $currentBranch
+            }
+        } elseif (($hotfixBranch -split "/")[1] -eq $null) {
+            $hotfixBranch = "hotfix/$hotfixBranch" 
+        }
+
         Set-Branch $m
         Update-BranchFrom $hotfixBranch -merge -noff
         Remove-Branch $hotfixBranch
         $tag = ($hotfixBranch -split "/")[1]
         New-Tag $tag
+
+        Write-Host "Will push..."
+        Pause
+        Write-Host "Continuing..."
+        #git push --follow-tags
     }
 }
 
@@ -144,8 +211,13 @@ function Start-Release {
         [Parameter(Mandatory=$false)][switch]$useDate
     )
     process { 
-        Set-Branch $m
-        $version = (gitversion | convertFrom-json)
+        Set-Branch $d
+        try {
+            $version = (gitversion /verbosity Quiet /nofetch /output json | convertFrom-json)
+        } catch {
+            return
+        }
+
         
         if ($useDate) {
             $tday = Get-Date
@@ -175,19 +247,36 @@ function Start-Release {
 function Complete-Release {
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory=$true)]$releaseBranch
+        [Parameter(Mandatory=$false)]$releaseBranch
     ) 
     process { 
+        if ($releaseBranch -eq $null) {
+            $currentBranch = (git branch --show-current)
+            $names = $currentBranch -split "/"
+            if ($names[0] -eq "release") {
+                $releaseBranch = $currentBranch
+            }
+        }
+
         Set-Branch $m
 
         $name = ($releaseBranch -split "/")[1]
+        if ($name -eq $null) {
+            $name = $releaseBranch
+            $releaseBranch = "release/$releaseBranch"
+        }
 
         Update-BranchFrom $releaseBranch -merge -noff
         New-Tag $name
 
         Set-Branch $d
-        Update-BranchFrom $m -rebase
+        Update-BranchFrom $m -merge
         Remove-Branch $releaseBranch
+
+        Write-Host "Will push all..."
+        Pause
+        Write-Host "Continuing..."
+        #git push --all --follow-tags
     }
 }
 
